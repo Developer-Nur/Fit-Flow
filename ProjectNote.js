85aa868982b6ef47caee66dc3fec0dafe291f81e
Developer Nur <nursweb6@gmail.com>
2024-07-12 21:08:14 +0600

    STAGING branch created to perform pull requests

---
+++ ProjectNote.js
@@ -0,0 +1,4 @@
+/*
+This is a STAGING branch. We will push our code to this (STAGING) branch consistently and perform pull requests to the STAGING branch until the project is completed. Once our project is ready, we will merge the STAGING branch into the main branch and deploy.
+
+*/  
