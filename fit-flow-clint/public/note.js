61c937fbc6029be372b603fb9a44b2b5ee00fa3e
Developer Nur <nursweb6@gmail.com>
2024-07-12 17:39:25 +0600

    first commit

---
+++ fit-flow-clint/public/note.js
@@ -0,0 +1,4 @@
+/**
+ *React-router-dom and tailwind css are already 
+  installedCheck the package.json for dependencies and feel free to install any other necessary packages. 
+ **/
---
+++ fit-flow-server/index.js
@@ -0,0 +1,6 @@
+
+
+
+/**
+ * Express, body-parser, cors, dotenv, mongodb, and jsonwebtoken are already installed. Check the package.json for dependencies and feel free to install any other necessary packages.
+ **/
